9a4cf4abc20cd93e34a570eff0ea3b9c07574218
Leolai6-7 <112496602+Leolai6-7@users.noreply.github.com>
2024-05-21 16:22:38 +0800

    add api.py

--- api.py
+++ api.py
@@ -1,4 +1,19 @@
+# 導入模組
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+
+    
+api = Api(app)
+
+class Helloworld(Resource):
+    def get(self):
+        return {
+            'message':'hello,world'    
+               }
+        
+api.add_resource(Helloworld,"/")
+
 if __name__ == '__main__':
     app.run()
