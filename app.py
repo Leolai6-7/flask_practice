09dde97231794e731abf78e88694fb1356429345
Leolai6-7 <112496602+Leolai6-7@users.noreply.github.com>
2024-05-21 14:57:52 +0800

    construct home and hello paage

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
+def home():
+    return 'This is Home Page!'
+@app.route("/hello")
 def hello():
-    return 'Hello World!'
+    return "Hello World! This is Hello Page "
 
 if __name__ == '__main__':
     app.run()
